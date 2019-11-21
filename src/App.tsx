bf156450da70cd4456b61e604f0052f22bb58009
Snorre Garmann <snorre@enso.no>
2019-11-21 11:34:43 +0100

    more authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { firebaseConfig } from '../src/firebase.config';
 
 const App: React.FC = () => {
+  var googleProvider:firebase.auth.GoogleAuthProvider;
+  const [email, setEmail] = useState<string>('');
+  function signIn(){
+    firebase.auth().signInWithPopup(googleProvider).then((result: any)=> {
+      if(result != null && result.credential != null){
+        console.log(result);
+      }            
+    });
+  }
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+    // Update the document title using the browser API
+    googleProvider = new firebase.auth.GoogleAuthProvider();
+    googleProvider.addScope('email');
+
+    var user = firebase.auth().currentUser;
+
+    if(!user){
+      console.log(user);
+    }
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +44,7 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <button onClick={signIn}>Sign in</button>
     </div>
   );
 }
---
+++ src/firebase.config.tsx
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBd9oY0alRMmp3HsxXBYHHY6zGfS-qGgVg",
+    authDomain: "julekalender-4617e.firebaseapp.com",
+    databaseURL: "https://julekalender-4617e.firebaseio.com",
+    projectId: "julekalender-4617e",
+    storageBucket: "julekalender-4617e.appspot.com",
+    messagingSenderId: "516885093234",
+    appId: "1:516885093234:web:0597c79f2d383fead31beb",
+    measurementId: "G-QMT6XF4GJ7"
+  };
