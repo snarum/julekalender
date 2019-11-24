ed6c2642778b65724634ebb7b902979543b0a160
Snorre Garmann <5821202+snarum@users.noreply.github.com>
2019-11-24 14:22:17 +0100

    finally fetch works

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,31 +1,47 @@
 import * as functions from 'firebase-functions';
 import * as adm from 'firebase-admin';
+//const cors = require('cors')({ origin: true });
 
+adm.initializeApp({
+    credential: adm.credential.cert(require('../secrets/key.json'))
+});
 
-adm.initializeApp();
 export const answer = functions.https.onRequest(async (req, res) => {
-
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.set('Access-Control-Allow-Headers', 'authorization,content-type')
     const a = req.headers.authorization;
     if (a === undefined || !a.startsWith("Bearer ")) {
+        console.log('her-1');
         res.json('');
     }
-    else
-    {
-        const p = a.replace("Bearer ","");
-        const token =  await adm.auth().verifyIdToken(p);
-        if(token === null || token.email === null)
-        {
+    else {
+        console.log('her0');
+        
+        const p = a.replace("Bearer ", "");
+        const token = await adm.auth().verifyIdToken(p);
+        if (token === null || token.email === null) {
             res.json('')
         }
+        const lukeParam = parseInt(req.query["luke"]);
+        const svarParam = parseInt(req.query["svar"]);
         const svarRef = adm.firestore().collection("svar");
-        const query = svarRef.where("luke", "==", parseInt(req.query["luke"])).where("hvem","==",token.email);
+        const query = svarRef.where("luke", "==", lukeParam).where("hvem", "==", token.email);
+        console.log('her1');
+
         const svar = await query.get();
+        console.log('her');
+
         if (svar.docs.length === 1) {
             const p2 = await svar.docs[0].data();
-            res.json(p2.hvor)
+            if (p2.svar === svarParam)
+                res.json(p2.hvor);
+            else
+                res.json('');
         }
         else {
             res.json('');
         }
     }
 });
+
+//app.use(cors);
--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import { Card, Dialog, DialogTitle, DialogContent, DialogContentText, TextField } from '@material-ui/core';
+import { Card, Dialog, DialogTitle, DialogContent, DialogContentText, RadioGroup, FormControlLabel, Radio, DialogActions } from '@material-ui/core';
 
-export class Luke{
-  constructor(public spørsmål: string){};
+export class Luke {
+  constructor(public spørsmål: string) { };
 }
 
 const App: React.FC = props => {
@@ -22,11 +22,11 @@ const App: React.FC = props => {
   const [open, setOpen] = React.useState(false);
   const [luke, setLuke] = React.useState<Luke>(new Luke(''));
   const [currentDay, setCurrentDay] = React.useState(0);
-
-  const handleClickOpen = async (day:number) => {
-    const luke = await firebase.firestore().collection("luker").where("dag","==",day);
+  const [answer, setAnswer] = React.useState('');
+  const handleClickOpen = async (day: number) => {
+    const luke = await firebase.firestore().collection("luker").where("dag", "==", day);
     const snap = await luke.get();
-    if(snap.docs.length !== 1)
+    if (snap.docs.length !== 1)
       return;
     setLuke(snap.docs[0].data() as Luke);
     setCurrentDay(day);
@@ -36,19 +36,46 @@ const App: React.FC = props => {
   const handleClose = () => {
     setOpen(false);
   };
+  const handleSvar = async () => {
+    setOpen(false);
+    const user = firebase.auth().currentUser;
+    if (user !== null) {
+      var t = await user.getIdToken();
+      fetch('http://localhost:5000/julekalender-4617e/us-central1/answer?luke=1&svar=2',
+        {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${t}`,
+          }
+        }
+      ).then(response => response.json())
+        .then(data => console.log(data));;
+    }
+  };
 
-  function signIn(){
-    firebase.auth().signInWithPopup(provider).then((result: any)=> {
-      if(result != null && result.credential != null){
-        
+
+  const getDisabled = (day: number): boolean => {
+    if (email === '')
+      return true;
+    return false;
+  }
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setAnswer(event.target.value);
+  };
+  function signIn() {
+    firebase.auth().signInWithPopup(provider).then((result: any) => {
+      if (result != null && result.credential != null) {
+
         setEmail(result.additionalUserInfo.profile['email']);
         const user = firebase.auth().currentUser;
-        if(user !== null){
-          user.getIdToken(true).then(x=>{
+        if (user !== null) {
+          user.getIdToken(true).then(x => {
             console.log(x);
-          });          
+          });
         }
-      }            
+      }
     });
   }
   useEffect(() => {
@@ -57,59 +84,65 @@ const App: React.FC = props => {
     provider.addScope('email');
     setProvider(provider);
 
-  },[provider]);
+  }, [provider]);
 
-  const days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+  const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
   return (
     <div>
-    <Container maxWidth="lg">
-    <Toolbar >
-      <Grid container className={styles.toolbarButtons}>
-        <Grid item></Grid>
-        <Grid item>
+      <Container maxWidth="lg">
+        <Toolbar >
+          <Grid container className={styles.toolbarButtons}>
+            <Grid item></Grid>
+            <Grid item>
 
-            
-      {email === '' &&
-      <Button variant="outlined" size="small" onClick={signIn}>
-        sign in
+
+              {email === '' &&
+                <Button variant="outlined" size="small" onClick={signIn}>
+                  sign in
       </Button>
-      }
-      {email !== '' && 
-      <div>{email}</div>
-      }
-      </Grid>
-      </Grid>
-    </Toolbar>
-    <Grid container>
-      <Grid item className={styles.alldays}>
-        {days.map(i=>{return(
-                  <Card className={styles.day} key={i}>
-                  <Button onClick={()=>handleClickOpen(i)}>{i}</Button>
-                  </Card>
-        )})}
+              }
+              {email !== '' &&
+                <div>{email}</div>
+              }
+            </Grid>
+          </Grid>
+        </Toolbar>
+        <Grid container>
+          <Grid item className={styles.alldays}>
+            {days.map(i => {
+              return (
+                <Card className={styles.day} key={i}>
+                  <Button onClick={() => handleClickOpen(i)} disabled={getDisabled(i)}>{i}</Button>
+                </Card>
+              )
+            })}
 
-      </Grid>
-    </Grid>
-    </Container>
-          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">Luke {currentDay}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              {luke.spørsmål}
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
-              fullWidth
-            />
-          </DialogContent>
-        </Dialog>
-        </div>
-      );
+          </Grid>
+        </Grid>
+      </Container>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Luke {currentDay}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {luke.spørsmål}
+          </DialogContentText>
+          <RadioGroup aria-label="gender" name="gender1" value={answer} onChange={handleChange}>
+            <FormControlLabel value="female" control={<Radio />} label="Female" />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSvar} color="primary">
+            Svar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 
